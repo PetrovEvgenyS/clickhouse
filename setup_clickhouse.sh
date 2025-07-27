80f8cdbdc58fa84e379cb3e3f928c8a156c211b9
Petrov Evgeny <PetrovE2@ya.ru>
2025-07-27 03:53:14 +0300

    update setup_clickhouse.sh

--- setup_clickhouse.sh
+++ setup_clickhouse.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # --- Переменные ---
+CLICKHOUSE_IP="$1"                  # IP-адрес сервера, с которого будет принимать запросы.
+CONFIG="/etc/clickhouse-server/config.d/my-config.xml"  # Конфиг с изменениями ClickHouse.
 
 ### ЦВЕТА ###
 ESC=$(printf '\033') RESET="${ESC}[0m" MAGENTA="${ESC}[35m" RED="${ESC}[31m" GREEN="${ESC}[32m"
@@ -20,6 +22,16 @@ if [ -z "$SUDO_USER" ]; then
     exit 1
 fi
 
+# --- Проверка наличия аргументов ---
+if [ -z "$1" ]; then
+  errorprint "Ошибка: не указан обязательный аргумент."
+  echo "Пожалуйста, укажите IP-адрес сервера, с которого будет принимать запросы."
+  echo "Использование: sudo $0 <CLICKHOUSE_IP>"
+  echo "Пример: sudo $0 10.100.10.1"
+  exit 1
+fi
+
+# --- Установка ClickHouse ---
 magentaprint "Настройка репозитория RPM:"
 dnf install -y yum-utils
 yum-config-manager --add-repo https://packages.clickhouse.com/rpm/clickhouse.repo
@@ -31,8 +43,25 @@ magentaprint "Запуск сервера ClickHouse:"
 systemctl enable --now clickhouse-server
 systemctl status clickhouse-server --no-page
 
+magentaprint "Версия ClickHouse:"
+clickhouse-server --version
+
+magentaprint "Логи ClickHouse:"
+ls -lah /var/log/clickhouse-server/*
+
+# --- Настройка прослушивания на $CLICKHOUSE_IP через config.d ---
+magentaprint "Настройка ClickHouse на прослушивание на интерфейсе ($CLICKHOUSE_IP) через config.d:"
+cat <<EOF > $CONFIG
+<yandex>
+    <listen_host>$CLICKHOUSE_IP</listen_host>
+</yandex>
+EOF
+
 magentaprint "Чтобы запустить клиента ClickHouse, выполните:"
 echo "clickhouse-client"
 
 magentaprint "Если вы установили пароль для вашего сервера, вам нужно будет выполнить:"
 echo "clickhouse-client --password"
+
+magentaprint "Пример, подключеник к ClickHouse:"
+echo "clickhouse-client -h <hostname> -u <username> --password"
